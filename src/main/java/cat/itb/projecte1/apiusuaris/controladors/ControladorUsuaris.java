06017cb77ee1528ad6ed06333b3d4714a818d7b9
mmadridejos-code <montse.madridejos@itb.cat>
2021-11-09 11:33:45 +0100

    cors

--- src/main/java/cat/itb/projecte1/apiusuaris/controladors/ControladorUsuaris.java
+++ src/main/java/cat/itb/projecte1/apiusuaris/controladors/ControladorUsuaris.java
@@ -17,16 +17,43 @@ public class ControladorUsuaris {
     //pel seu corresponent retornant un ResponseEntity
 
 
+    /*  exemple de control cors a nivell de mètode  */
+    @CrossOrigin(origins="http://localhost:9001")
     @GetMapping("/usuaris")
-    public List<Usuari> llistarUsuaris(){
-        return serveiUsuaris.llistarUsuaris();
+    public List<Usuari> llistarUsuaris(@RequestParam (value="sou", required=false) String sou){
+        if(sou==null){
+            return serveiUsuaris.llistarUsuaris();
+        }
+        else return serveiUsuaris.llistatPerSouMenorA(Double.parseDouble(sou));
     }
+
+
+    //requestparam are required by default
+    //l'endpoint és únic, crea ambigüitat
+//    @GetMapping("/usuaris")
+//    public List<Usuari> llistarMenorsAUnSou(@RequestParam (value="sou") double sou){
+//        return serveiUsuaris.llistatPerSouMenorA(sou);
+//    }
+
+
 
     @GetMapping("/usuaris/{id}")
     public Usuari consultarUsuari(@PathVariable String id)
     {
         return serveiUsuaris.consultarUsuari(id);
     }
+
+    @GetMapping("/usuaris/rol/{rol}")
+    public List<Usuari> llistarUsuarisPerRol(@PathVariable String rol){
+        return serveiUsuaris.llistarUsuarisPerRol(rol);
+    }
+
+    @GetMapping("/usuaris/comptar/{rol}")
+    public long comptarPerRol(@PathVariable String rol){
+        return serveiUsuaris.comptarPerRol(rol);
+    }
+
+
 
     @PostMapping("/usuaris")
     public Usuari crearUsuari(@RequestBody Usuari nou){
